31d6ca7064f209ecd42bcf5c13520b86798decb4
Juwon Hwang <wndnjs719@gmail.com>
2023-07-18 15:24:53 +0900

    정규 url 모델 변경, 템플릿 변경

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -40,6 +41,10 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish']),
         ]
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.id])
 
     def __str__(self):
         return self.title

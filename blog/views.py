edad56ae933b2b92914ee2859438058e47772a5f
Juwon Hwang <wndnjs719@gmail.com>
2023-07-19 13:19:43 +0900

    클래스형 뷰 추가, url 변경

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from blog.models import Post
 
 
@@ -29,3 +31,10 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+class PostListView(ListView):
+    # model = Post <- Post.objects.all() 쿼리를 실행
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'

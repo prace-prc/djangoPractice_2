29a97fa6ded4aaa88e829cc5d422e6d1a21c0995
Juwon Hwang <wndnjs719@gmail.com>
2023-07-19 15:22:37 +0900

    post_share 뷰 로직 수정, 메시지 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
@@ -36,13 +37,23 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = f"{cd['name']}님이 {post.title}을 추천합니다."
+            message = f"{post.title}을 다음에서 읽어보세요\n\n." \
+                      f"{cd['name']}님의 의견:{cd['comments']}"
+            send_mail(subject,message,'myemail@gmail.com', [cd['to']])
+            sent = True
+
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 
 class PostListView(ListView):

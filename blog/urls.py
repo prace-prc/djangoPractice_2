7f04dcefe7b1a2f72ae624c6e7d018bcbaa92687
Juwon Hwang <wndnjs719@gmail.com>
2023-07-19 10:47:28 +0900

    Pagination 추가

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path(r'<int:year>/<int:month>/<int:day>/<str:post>/', views.post_detail, name='post_detail'),
 ]

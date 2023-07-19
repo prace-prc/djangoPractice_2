aa39959444b8abf01ae4e19868e746811215ae80
Juwon Hwang <wndnjs719@gmail.com>
2023-07-19 09:22:38 +0900

    URL 패턴 모델, 뷰 수정

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-
 from blog.models import Post
 
 
@@ -10,10 +9,14 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     # try:
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404("No Post found")
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})

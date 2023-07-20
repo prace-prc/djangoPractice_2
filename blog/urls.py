64ccff85401f940a0d201e7a7760499f66e4e3e0
Juwon Hwang <wndnjs719@gmail.com>
2023-07-20 13:33:56 +0900

    tag 기능 추가 위한 list.html 수정, url 경로 추가

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     # path('', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
+    path('tag/<str:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<str:post>/', views.post_detail,
          name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,20 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
+from taggit.models import Tag
 
 from blog.forms import EmailPostForm, CommentForm
 from blog.models import Post
 
 
 # Create your views here.
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     paginator = Paginator(post_list, 3, orphans=1)
     page_number = request.GET.get('page', 1)
     try:
@@ -20,7 +26,8 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts,
+                                                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):

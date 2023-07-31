73a514f7d597ce010e49e2971fd3bcd5d849657e
Juwon Hwang <wndnjs719@gmail.com>
2023-07-31 10:14:02 +0900

    [기능 추가] 사용자 정보 등록 폼, 뷰 작성

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from account.forms import LoginForm
+from account.forms import LoginForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -27,6 +27,26 @@ def user_login(request):
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
 
+
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})

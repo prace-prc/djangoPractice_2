29bdf3af76b7e217ed2185a2bd917bebe8f15be1
Juwon Hwang <wndnjs719@gmail.com>
2023-08-04 10:09:58 +0900

    [기능 추가] create action 유틸 account 뷰에 사용

--- account/views.py
+++ account/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.http import require_POST
 
 from .models import Profile, Contact
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from actions.utils import create_action
 
 
 # Create your views here.
@@ -47,6 +48,7 @@ def register(request):
             )
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -106,6 +108,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user,'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({'status': 'ok'})

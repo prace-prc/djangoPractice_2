6aabeeb0b4181201f8d892f8aff5c5e7701b3e0e
Juwon Hwang <wndnjs719@gmail.com>
2023-08-10 11:08:34 +0900

    [기능 추가] 주문 비동기 작업 추가

--- orders/views.py
+++ orders/views.py
@@ -13,10 +13,11 @@ def order_create(request):
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
-            for item in cart: OrderItem.objects.create(order=order,
-                                                       product=item['product'],
-                                                       price=item['price'],
-                                                       quantity=item['quantity'])
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
             cart.clear()
             order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})

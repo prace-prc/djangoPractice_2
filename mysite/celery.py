1430be1bb621fe71a0a60d96201366596746c3b5
Juwon Hwang <wndnjs719@gmail.com>
2023-08-10 10:42:31 +0900

    [오류 수정] celery 모듈 설정 오류 수정

--- mysite/celery.py
+++ mysite/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','myshop.settings')
-app = Celery('myshop')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','mysite.settings')
+app = Celery('mysite')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()

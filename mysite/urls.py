439f646adb2b759c8d3b9d5b902c1dc251234e09
Juwon Hwang <wndnjs719@gmail.com>
2023-08-14 13:07:50 +0900

    [기능 추가] 결제 url 구성

--- mysite/urls.py
+++ mysite/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('blog/', include('blog.urls', namespace='blog')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('payment/', include('payment.urls', namespace='payment')),
     path('', include('shop.urls', namespace='shop')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),

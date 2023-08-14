6f3ac445ae35510e72c8d2082481450731938b10
Juwon Hwang <wndnjs719@gmail.com>
2023-08-14 10:04:30 +0900

    [기능 추가] celery 설정 윈도우에서 오류 안나게

--- mysite/celery.py
+++ mysite/celery.py
@@ -2,7 +2,9 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
+
 app = Celery('mysite')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()

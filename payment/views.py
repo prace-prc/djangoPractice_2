f42f389cf5f9edc35a289c93072e9e176f5fba26
Juwon Hwang <wndnjs719@gmail.com>
2023-08-14 13:06:45 +0900

    [기능 추가] 결제 프로세스, 완료, 취소 뷰

--- payment/views.py
+++ payment/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.shortcuts import render, get_object_or_404, redirect
 from orders.models import Order
 import stripe
+from _decimal import Decimal
 
 # Create your views here.
 
@@ -22,6 +23,25 @@ def payment_process(request):
                         'cancel_url': cancel_url,
                         'line_items': []}
         session = stripe.checkout.Session.create(**session_data)
+        for item in order.items.all():
+            session_data['line_items'].append({
+                'price_data': {
+                    'unit_amount': int(item.price * Decimal('100')),
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': item.product.name,
+                    },
+                }, 'quantity': item.quantity,
+            })
+        session = stripe.checkout.Session.create(**session_data)
         return redirect(session.url, code=303)
     else:
         return render(request, 'payment/process.html', locals())
+
+
+def payment_completed(request):
+    return render(request, 'payment/completed.html')
+
+
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')

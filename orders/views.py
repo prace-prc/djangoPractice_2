e0ce245dabd4b350cd7c93c0c6f596d031214b0e
Juwon Hwang <wndnjs719@gmail.com>
2023-08-16 14:31:33 +0900

    [기능 추가] 관리 사이트 확장

--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from cart.cart import Cart
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
 from .tasks import order_created
 
 
 # Create your views here.
 def order_create(request):
     cart = Cart(request)
-
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
         if form.is_valid():
@@ -25,5 +25,12 @@ def order_create(request):
             return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
-
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})

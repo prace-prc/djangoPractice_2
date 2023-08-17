26c960dafa519702e8a72a5cad1e3b5cc473baf8
Juwon Hwang <wndnjs719@gmail.com>
2023-08-17 13:09:13 +0900

    [기능 작성] 쿠폰 사용 폼, 뷰, url 작성

--- mysite/urls.py
+++ mysite/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('payment/', include('payment.urls', namespace='payment')),
+    path('coupons/', include('coupons.urls', namespace='coupons')),
     path('', include('shop.urls', namespace='shop')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
